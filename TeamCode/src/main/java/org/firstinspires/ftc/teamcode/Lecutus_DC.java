0d7465bc382be1261f55dac4f2354b2c671e2e3c
Nyle Pope <popen20@nsacademy.org>
2019-04-11 09:37:30 -0400

    4/11/19 A club

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lecutus_DC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lecutus_DC.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-@TeleOp(name="Jack's Super Secret Test Code", group="Iterative Opmode")
+@TeleOp(name="Lecutus DC", group="Iterative Opmode")
 //@Disabled
 public class Lecutus_DC extends OpMode
 {
@@ -25,9 +25,12 @@ public class Lecutus_DC extends OpMode
     private Servo leftcollector;
     private Servo rightcollector;
     private Servo scorer;
+    int inCrater = 0;
+
 
     public void init()
     {
+
         frontLeft  = hardwareMap.get(DcMotor.class, "front_left");
         frontLeft.setDirection(DcMotor.Direction.FORWARD);
 
@@ -49,8 +52,19 @@ public class Lecutus_DC extends OpMode
         extender = hardwareMap.get(DcMotor.class, "extender");
         extender.setDirection(DcMotor.Direction.REVERSE);
 
+        scorer = hardwareMap.get(Servo.class, "scorer");
+        scorer.setPosition(0.1);
+
+        leftcollector = hardwareMap.get(Servo.class, "leftcollector");
+        leftcollector.setPosition(1);
+
+        rightcollector= hardwareMap.get(Servo.class, "rightcollector");
+        rightcollector.setPosition(0);
+
         spinner = hardwareMap.get(DcMotor.class, "spinner");
         spinner.setDirection(DcMotor.Direction.FORWARD);
+
+
     }
 
     @Override
@@ -60,54 +74,81 @@ public class Lecutus_DC extends OpMode
     }
     public void loop()
     {
-        // Wheel motor power values. Right motor direction is reversed.
+        telemetry.addData("Running", " :)");
+        telemetry.update();
+        telemetry.clear();
+        telemetry.addData("Running",";)" );
+        telemetry.update();
+        telemetry.clear();
 
-        double frontleftWheelPower = ((gamepad1.left_stick_y - gamepad1.left_stick_x) + (gamepad1.right_stick_x));
-        double frontrightWheelPower = ((gamepad1.left_stick_y - gamepad1.left_stick_x) - (gamepad1.right_stick_x));
-        double backleftWheelPower = ((gamepad1.left_stick_y + gamepad1.left_stick_x) + (gamepad1.right_stick_x));
-        double backrightWheelPower = ((gamepad1.left_stick_y + gamepad1.left_stick_x) - (gamepad1.right_stick_x));
+
+        // Wheel motor power values. Right motor direction is reversed.
+        double frontleftWheelPower = ((gamepad1.left_stick_y - gamepad1.left_stick_x) - (gamepad1.right_stick_x));
+        double frontrightWheelPower = ((gamepad1.left_stick_y + gamepad1.left_stick_x) + (gamepad1.right_stick_x));
+        double backleftWheelPower = ((gamepad1.left_stick_y + gamepad1.left_stick_x) - (gamepad1.right_stick_x));
+        double backrightWheelPower = ((gamepad1.left_stick_y - gamepad1.left_stick_x) + (gamepad1.right_stick_x));
         if(gamepad1.dpad_up)
         {
-            actuator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            /*actuator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             actuator.setTargetPosition(1000);
             actuator.setPower(1);
             while(actuator.isBusy())
             {
 
-            }
-            actuator.setPower(0);
+            }*/
+            actuator.setPower(1);
 
         }
-        if(gamepad1.dpad_down)
+         else if(gamepad1.dpad_down)
         {
-            actuator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            actuator.setTargetPosition(-1000);
-            actuator.setPower(-1);
-            while(actuator.isBusy())
-            {
 
-            }
-            actuator.setPower(0);
+            actuator.setPower(-1);
 
         }
+        else actuator.setPower(0);
         slide.setPower(gamepad1.left_trigger-gamepad1.right_trigger);
-        extender.setPower(-gamepad2.right_stick_y);
+        if (gamepad2.right_stick_y>0)
+            extender.setPower(0.6 *gamepad2.right_stick_y);
+        else extender.setPower(gamepad2.right_stick_y);
+
+
+        if(gamepad2.dpad_up) //transfer
+        {
+            leftcollector.setPosition(.2);
+            rightcollector.setPosition(.8);
+        }
+        if(gamepad2.dpad_right||gamepad2.dpad_left) //up
+        {
+            leftcollector.setPosition(0.5);
+            rightcollector.setPosition(0.5);
+        }
+        if (gamepad2.dpad_down) //collect
+        {
+            leftcollector.setPosition(1);
+            rightcollector.setPosition(0);
+        }
+        slide.getZeroPowerBehavior();
+
         if(gamepad2.right_bumper)
             spinner.setPower(1);
         if(gamepad2.left_bumper)
-            spinner.setPower(-0.25);
+            spinner.setPower(-1);
         if(gamepad2.x)
             spinner.setPower(0);
 
-        if(gamepad1.a)
-            scorer.setPosition(0.75);
-        else scorer.setPosition(0);
+
+
+
 
 
         frontLeft.setPower(frontleftWheelPower);
         frontRight.setPower(frontrightWheelPower);
         backLeft.setPower(backleftWheelPower);
         backRight.setPower(backrightWheelPower);
+
+        if(gamepad1.a) scorer.setPosition(0.5);
+        else if (gamepad1.b)scorer.setPosition(0.35);
+        else scorer.setPosition(0.1);
     }
     @Override
     public void stop() { }
